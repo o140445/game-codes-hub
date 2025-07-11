4c9c16806508b877e31456a2bd6e05c31f7678c4
o140445 <o140445s@gmail.com>
2025-07-11 15:55:25 +0800

    logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -42,10 +42,10 @@
             <div class="md:col-span-2 bg-gray-900 rounded-lg shadow-lg p-3 sm:p-4 flex flex-col md:flex-row gap-3 sm:gap-4
             transition-all duration-200 hover:scale-105 hover:shadow-xl group cursor-pointer border border-gray-700 relative">
                 <a href="{{ route('games.show', $data['first']['slug']) }}" class="w-full h-full ">
-                    <img src="{{ asset($data['first']['image']) }}" alt="{{ $data['first']['slug'] }}" class="rounded-lg w-full h-full object-cover">
+                    <img src="{{ asset($data['first']['image']) }}" alt="{{ $data['first']['slug'] }}" class="rounded-lg w-full md:h-96 h-56 object-cover">
                 </a>
                 <!-- 悬浮在图片上 左下角 -->
-                <div class="absolute bottom-8 left-5 flex items-center justify-center">
+                <div class="absolute md:bottom-4 md:left-5 left-4 bottom-2 flex items-center justify-center">
                     <h1 class=" text-3xl sm:text-2xl font-bold mb-2 transition-colors duration-200 group-hover:text-yellow-400 text-white">{{ $data['first']['name'] }}</h1>
                 </div>
 
